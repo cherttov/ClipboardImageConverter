87f847edd5b8d0e35efd2170da9416f73e5079f8
Parek4Life <125895505+Cherttov@users.noreply.github.com>
2026-09-12 20:50:08 +0200

    Added MacosClipboardService - not implemented

---
+++ src/services/clipboard/MacosClipboardService.cs
@@ -0,0 +1,17 @@
+﻿using ClipboardImageConverter.src.models;
+
+namespace ClipboardImageConverter.src.services.clipboard
+{
+	public class MacosClipboardService : IClipboardService
+	{
+		public ClipboardImageResult? GetClipboardImageData()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void PushClipboardData(byte[] data, string ext, string origName)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
